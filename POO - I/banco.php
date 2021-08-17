c57b91a3de854cc7e9e4d97cad7072f587acc16d
Matheus Nunes <ortiz.matheus2017@gmail.com>
2021-08-17 11:20:10 -0300

    Inclusão de autoload nos arquivos.

--- POO - I/banco.php
+++ POO - I/banco.php
@@ -1,13 +1,27 @@
 <?php
 
-require_once 'Conta.php';
-require_once 'Titular.php';
-require_once 'Endereco.php';
-require_once 'Cpf.php';
+require_once 'autoload.php';
 
-$endereco = new Endereco('Soledade','Missoes','13 de Maio','1434');
-$cpf = new Cpf('123.888.999-90');
-$titular = new Titular($cpf, 'Fernanda', $endereco);
-$conta = new Conta($titular);
-var_dump($conta);
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Conta\Conta;
 
+$endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
+$primeiraConta = new Conta($vinicius);
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
+
+$outroEndereco = new Endereco('A', 'b', 'c', '1D');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
